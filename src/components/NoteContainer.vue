0396e3cca15d0ba821db57d0bbadecd0e65ea891
Micah Sterling <msterling89@gmail.com>
2021-04-18 11:16:23 -0500

    add noteSelector component

--- src/components/NoteContainer.vue
+++ src/components/NoteContainer.vue
@@ -1,19 +1,5 @@
 <template>
-  	<div class="note-container">
-    <div class="note-selectors">
-      <div class="note-selector active">
-        <p class="note-selector-title">First note...</p>
-        <p class="note-selector-timestamp">Timestamp here...</p>
-      </div>
-      <div class="note-selector">
-        <p class="note-selector-title">Second note...</p>
-        <p class="note-selector-timestamp">Timestamp here...</p>
-      </div>
-      <div class="note-selector">
-        <p class="note-selector-title">Third note...</p>
-        <p class="note-selector-timestamp">Timestamp here...</p>
-      </div>
-    </div>
+  <div class="note-container">
     <div class="note-editor">
       <p class="note-editor-info">Timestamp here...</p>
       <textarea class="note-editor-input">
@@ -23,3 +9,12 @@
     </div>
   </div>
 </template>
+
+<script>
+import NoteSelectors from "./NoteSelector.vue";
+export default {
+  components: {
+    NoteSelectors,
+  },
+};
+</script>
---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="note-selectors">
+   <NoteSelector />
+  </div>
+</template>
+
+<script>
+import NoteSelector from "./NoteSelector.vue";
+export default {
+  components: {
+    NoteSelector,
+  },
+};
+</script>
